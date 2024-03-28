6616bc7bdda2a83a5b5d5c0df6e26160dc8ddb69
Victor <polyakov.victor21@gmail.com>
2024-03-28 17:12:26 +0200

    front/ add about page

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -7,22 +7,23 @@ const Navbar: any = () => {
             <div className="container">
                 <Link to="/"><a className="navbar-brand">Онлайн Банкінг</a></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon">
+                </span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <Link to="/"><a className="nav-link" href="#">Головна</a></Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/service"><a className="nav-link">Послуги</a></Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/service"><a className="nav-link" href="#">Про нас</a></Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/profile"><a className="nav-link" href="#">Особистий кабінет</a></Link>
-                </li>
+                    <li className="nav-item">
+                        <Link to="/"><a className="nav-link" href="#">Головна</a></Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/service"><a className="nav-link">Послуги</a></Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/about"><a className="nav-link" href="#">Про нас</a></Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/profile"><a className="nav-link" href="#">Особистий кабінет</a></Link>
+                    </li>
                 </ul>
             </div>
             </div>
--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -3,6 +3,7 @@ import HomePage from '../pages/home-page/home-page'
 import AuthPage from '../pages/auth-page/auth-page'
 import UserProfile from '../pages/user-profile/user-profile'
 import ServicePage from '../pages/service-page/service-page'
+import AboutPage from '../pages/about-page/about-page'
 
 
 const Router = () => {
@@ -12,6 +13,7 @@ const Router = () => {
             <Route element={<AuthPage />} path='/auth' />
             <Route element={<UserProfile />} path='/profile' />
             <Route element={<ServicePage />} path='/service' />
+            <Route element={<AboutPage />} path='/about' />
             <Route element={<div>Not found</div>} path='*'/>
         </Routes>
     </BrowserRouter>
