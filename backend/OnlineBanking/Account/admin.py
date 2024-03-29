5ac77c5fa43a02b19df17a708726fda5d6f10d09
Victor <polyakov.victor21@gmail.com>
2024-03-29 16:07:02 +0200

    django/ edit admin.py

--- backend/OnlineBanking/Account/admin.py
+++ backend/OnlineBanking/Account/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Account, Customer
+from Transaction.models import Transaction
 
-# Register your models here.
+
+class TransactionInline(admin.TabularInline):
+    model = Transaction
+    extra = 0
+
+
+class AccountInline(admin.StackedInline):
+    model = Account
+    inlines = [TransactionInline]
+    extra = 0
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    inlines = [AccountInline]
+    list_display = ('user', 'phone_number', 'address')
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [TransactionInline]
+    list_display = ('account_number', 'account_type', 'balance', 'customer')
--- backend/OnlineBanking/Transaction/admin.py
+++ backend/OnlineBanking/Transaction/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('account', 'transaction_type', 'amount', 'date', 'description')
+    list_filter = ('transaction_type', 'date')
+    search_fields = ('account__account_number', 'description')
