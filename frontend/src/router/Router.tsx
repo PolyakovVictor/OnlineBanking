3de835bf374313facde696afb13a16f71c0a4482
Victor <polyakov.victor21@gmail.com>
2024-04-21 01:42:20 +0300

    front/ fix bug

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,51 +1,60 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from '../pages/home-page/home-page'
-import AuthPage from '../pages/auth-page/auth-page'
-import UserProfile from '../pages/user-profile/user-profile'
-import ServicePage from '../pages/service-page/service-page'
-import AboutPage from '../pages/about-page/about-page'
-import ProtectedRoute from '../components/protected-route/protected-route'
-import { AuthService } from "../services/auth.service";
-
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from '../pages/home-page/home-page';
+import AuthPage from '../pages/auth-page/auth-page';
+import UserProfile from '../pages/user-profile/user-profile';
+import ServicePage from '../pages/service-page/service-page';
+import AboutPage from '../pages/about-page/about-page';
+import ProtectedRoute from '../components/protected-route/protected-route';
+import { AuthService } from '../services/auth.service';
 
 const Router = () => {
-    const isToken = () => {
-        return AuthService.getCookie('access_token') !== null;
-      };
-    
-    const isAuthenticated: boolean = isToken();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  
-    return (
-      <BrowserRouter>
-        <Routes>
-            <Route element={<HomePage />} path="/" />
-            <Route element={<AuthPage />} path="/auth" />
-            <Route 
-                element={  
-                    <ProtectedRoute
-                        isAuthenticated={isAuthenticated}
-                        authenticationPath="/auth"
-                        outlet={<UserProfile />}
-                    />
-                }
-                path="/profile"
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const accessToken = await AuthService.getCookie('access_token');
+        setIsAuthenticated(accessToken !== undefined);
+      } catch (error) {
+        console.error('Error when checking token:', error);
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<HomePage />} path="/" />
+        <Route element={<AuthPage />} path="/auth" />
+        <Route
+          element={
+            <ProtectedRoute
+              isAuthenticated={isAuthenticated}
+              authenticationPath="/auth"
+              outlet={<UserProfile />}
             />
-            <Route
-                element={
-                    <ProtectedRoute
-                        isAuthenticated={isAuthenticated}
-                        authenticationPath="/auth"
-                        outlet={<ServicePage />}
-                    />
-                }
-                path="/service"
+          }
+          path="/profile"
+        />
+        <Route
+          element={
+            <ProtectedRoute
+              isAuthenticated={isAuthenticated}
+              authenticationPath="/auth"
+              outlet={<ServicePage />}
             />
-          <Route element={<AboutPage />} path="/about" />
-          <Route element={<div>Not found</div>} path="*" />
-        </Routes>
-      </BrowserRouter>
-    );
-  };
-  
-  export default Router;
+          }
+          path="/service"
+        />
+        <Route element={<AboutPage />} path="/about" />
+        <Route element={<div>Not found</div>} path="*" />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
