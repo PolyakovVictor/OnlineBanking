822ea85d96994d856b1d2660253a1d2192529991
Victor <polyakov.victor21@gmail.com>
2024-06-06 11:13:28 +0300

    backend/ edit deposit logic

--- backend/OnlineBanking/Transaction/models.py
+++ backend/OnlineBanking/Transaction/models.py
@@ -5,8 +5,12 @@ from .services import transfer_funds
 
 
 class Transaction(models.Model):
-    from_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='outgoing_transactions')
-    to_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='incoming_transactions')
+    from_account = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name="outgoing_transactions"
+    )
+    to_account = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name="incoming_transactions"
+    )
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
     category = models.CharField(max_length=100, null=True, blank=True)
@@ -17,7 +21,9 @@ class Transaction(models.Model):
 
 
 class Deposit(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='deposits')
+    account = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name="deposits"
+    )
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     term = models.IntegerField()
     interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
@@ -29,9 +35,37 @@ class Deposit(models.Model):
     def __str__(self):
         return f"Deposit for {self.account.account_number} (Amount: {self.amount})"
 
+    def save(self, *args, **kwargs):
+        # Check if this is a new deposit
+        if not self.pk:
+            with transaction.atomic():
+                from_account = self.account
+                bank = Customer.objects.get(username="Bank")
+                bank_account = Account.objects.get(customer=bank)
+
+                # Transfer funds from user account to bank account
+                transfer_funds(from_account, bank_account, self.amount)
+
+                # Create the corresponding transaction
+                Transaction.objects.create(
+                    from_account=from_account,
+                    to_account=bank_account,
+                    amount=self.amount,
+                    date=timezone.now(),
+                    category="Deposit",
+                    description=f"Deposit amount from {from_account.account_number}",
+                )
+
+                super().save(*args, **kwargs)  # Save the deposit instance
+
+        else:
+            super().save(*args, **kwargs)
+
 
 class Credit(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='credits')
+    account = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name="credits"
+    )
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     term = models.IntegerField()
     interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
@@ -49,7 +83,7 @@ class Credit(models.Model):
         if not self.pk:
             with transaction.atomic():
                 super().save(*args, **kwargs)  # Save the credit instance first
-                bank = Customer.objects.get(username='Bank')
+                bank = Customer.objects.get(username="Bank")
 
                 bank_account = Account.objects.get(customer=bank)
 
@@ -59,8 +93,8 @@ class Credit(models.Model):
                     to_account=self.account,
                     amount=self.amount,
                     date=timezone.now(),
-                    category='Credit',
-                    description=f"Credit amount for {self.account.account_number}"
+                    category="Credit",
+                    description=f"Credit amount for {self.account.account_number}",
                 )
 
                 transfer_funds(bank_account, self.account, self.amount)
--- backend/OnlineBanking/Transaction/views.py
+++ backend/OnlineBanking/Transaction/views.py
@@ -5,7 +5,12 @@ from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .services import transfer_funds
-from .serializers import TopUpSerializer, TransactionSerializer, DepositSerializer, CreditSerializer
+from .serializers import (
+    TopUpSerializer,
+    TransactionSerializer,
+    DepositSerializer,
+    CreditSerializer,
+)
 from Account.models import Account, Customer
 from .models import Transaction, Deposit, Credit
 from rest_framework import generics
@@ -18,11 +23,11 @@ class TransactionViewSet(APIView):
     def post(self, request):
         serializer = TransactionSerializer(data=request.data)
         if serializer.is_valid():
-            from_account_number = serializer.data.get('from_account_number')
-            to_account_number = serializer.data.get('to_account_number')
-            amount = serializer.validated_data['amount']
-            category = serializer.validated_data.get('category', None)
-            description = serializer.validated_data.get('description', None)
+            from_account_number = serializer.data.get("from_account_number")
+            to_account_number = serializer.data.get("to_account_number")
+            amount = serializer.validated_data["amount"]
+            category = serializer.validated_data.get("category", None)
+            description = serializer.validated_data.get("description", None)
 
             try:
                 from_account = Account.objects.get(account_number=from_account_number)
@@ -34,12 +39,12 @@ class TransactionViewSet(APIView):
                     to_account=to_account,
                     amount=amount,
                     category=category,
-                    description=description
+                    description=description,
                 )
 
-                return Response({'success': True}, status=status.HTTP_200_OK)
+                return Response({"success": True}, status=status.HTTP_200_OK)
             except (ValueError, ObjectDoesNotExist) as e:
-                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -55,7 +60,10 @@ class UserTransactionsView(APIView):
         to_transactions = Transaction.objects.filter(to_account=account)
         from_serializer = TransactionSerializer(from_transactions, many=True)
         to_serializer = TransactionSerializer(to_transactions, many=True)
-        return Response({'sender': from_serializer.data, 'receiver': to_serializer.data}, status=status.HTTP_200_OK)
+        return Response(
+            {"sender": from_serializer.data, "receiver": to_serializer.data},
+            status=status.HTTP_200_OK,
+        )
 
 
 class DepositCreateView(generics.ListCreateAPIView):
@@ -94,15 +102,18 @@ class TopUpView(generics.CreateAPIView):
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            amount = serializer.validated_data['amount']
+            amount = serializer.validated_data["amount"]
             account = request.user.account
 
             try:
-                bank_customer = Customer.objects.get(username='Bank')
+                bank_customer = Customer.objects.get(username="Bank")
                 bank_account = Account.objects.get(customer=bank_customer)
 
                 if bank_account.balance < amount:
-                    return Response({'error': 'Insufficient funds in bank account'}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response(
+                        {"error": "Insufficient funds in bank account"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
 
                 transfer_funds(bank_account, account, amount)
 
@@ -110,13 +121,19 @@ class TopUpView(generics.CreateAPIView):
                     from_account=bank_account,
                     to_account=account,
                     amount=amount,
-                    category='TopUP',
-                    description=''
+                    category="TopUP",
+                    description="",
                 )
 
-                return Response({'success': True}, status=status.HTTP_200_OK)
+                return Response({"success": True}, status=status.HTTP_200_OK)
             except Customer.DoesNotExist:
-                return Response({'error': 'Bank customer does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Bank customer does not exist"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
             except Account.DoesNotExist:
-                return Response({'error': 'Bank account does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Bank account does not exist"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
